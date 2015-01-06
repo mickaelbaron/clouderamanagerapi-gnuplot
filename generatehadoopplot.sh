59549eecef1b1ee5c982795665cf574ffea1c404
Mickael BARON <baron.mickael@gmail.com>
2015-01-06 15:59:40 +0100

    Add target repositories for storing data and images

--- generatehadoopplot.sh
+++ generatehadoopplot.sh
@@ -2,18 +2,27 @@
 
 USER=admin
 PASSWORD=admin
-SERVER=TODO
+SERVER=http://s-virtualmachine2-lias:7180
 STARTDATE=$1
 FINISHDATE=$2
 shift 2
 
-wget -q --user=$USER --password=$PASSWORD -O clusteresutilization.csv "$SERVER/api/v7/timeseries?query=select stats(utilization_across_disks, avg) where category = CLUSTER&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
-wget -q --user=$USER --password=$PASSWORD -O clustercpuusage.csv "$SERVER/api/v7/timeseries?query=select stats(cpu_percent_across_hosts,avg) where category = CLUSTER&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
+outputImage="output_image"
+outputData="output_data"
+
+rm -rf $outputImage
+rm -rf $outputData
+
+mkdir $outputData -p
+mkdir $outputImage -p
+
+wget -q --user=$USER --password=$PASSWORD -O $outputData/clusterioutilization.csv "$SERVER/api/v7/timeseries?query=select stats(utilization_across_disks, avg) where category = CLUSTER&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
+wget -q --user=$USER --password=$PASSWORD -O $outputData/clustercpuusage.csv "$SERVER/api/v7/timeseries?query=select stats(cpu_percent_across_hosts,avg) where category = CLUSTER&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
 
 gnuplot << EOF
-set title "CPU & Disk usage during a WordCount MapReduce execution (4 nodes, 50 Gb bigfile.txt)"
+set title "CPU & Disk usage during a WordCount MapReduce execution ($# nodes, 50 Gb bigfile.txt)"
 set terminal "png" size 800,500
-set output "cluster.png"
+set output "$outputImage/mrclustercpuio.png"
 set datafile separator ','
 set xlabel "Time (hh:mm)"
 set ylabel "Percentage (%)"
@@ -21,7 +30,7 @@ set xtics format "%H:%M"
 set xdata time
 set yrange [0:100]
 set grid x y
-plot "clusteresutilization.csv" using (\$0*60):4 w l lt rgb "#FF0000" title 'Disk usage', "clustercpuusage.csv" using (\$0*60):4 w l lt rgb "#0000FF" title 'CPU Usage'
+plot "$outputData/clusterioutilization.csv" using (\$0*60):4 w l lt rgb "#FF0000" title 'Disk usage', "$outputData/clustercpuusage.csv" using (\$0*60):4 w l lt rgb "#0000FF" title 'CPU Usage'
 EOF
 
 commonglobalplot="set terminal pngcairo dashed size 800,500
@@ -42,28 +51,28 @@ set style line 5 linetype 1 linecolor rgb \"yellow\"
 set style line 6 linetype 1 linecolor rgb \"brown\"
 set style line 7 linetype 1 linecolor rgb \"cyan\""
 
-commonglobalesutilizationplot="
+commonglobalioutilizationplot="
 set title \"Disk usage during a WordCount MapReduce execution ($# nodes, 50 Gb bigfile.txt)\"
-set output \"mrglobalesutilization$#.png\"
+set output \"$outputImage/mrglobalioutilization$#.png\"
 
-plot \"clusteresutilization.csv\" using (\$0*60):4 ls 1 w l title 'Avg Disk utilization cluster',"
+plot \"$outputData/clusterioutilization.csv\" using (\$0*60):4 ls 1 w l title 'Avg Disk utilization cluster',"
 
 commonglobalcpuusageplot="
 set title \"CPU usage during a WordCount MapReduce execution ($# nodes, 50 Gb bigfile.txt)\"
-set output \"mrglobalcpuusage$#.png\"
+set output \"$outputImage/mrglobalcpuusage$#.png\"
 
-plot \"clustercpuusage.csv\" using (\$0*60):4 ls 1 w l title 'Avg CPU usage cluster',"
+plot \"$outputData/clustercpuusage.csv\" using (\$0*60):4 ls 1 w l title 'Avg CPU usage cluster',"
 
 for ((i=1;i<=$#;i++))
 do
 	nodename=${!i};
-	cpuusage_filename=$nodename."cpuusage.csv";
-	esutilization_filename=$nodename."esutilization.csv";
+	cpuusage_filename=$outputData/$nodename."cpuusage.csv";
+	ioutilization_filename=$outputData/$nodename."ioutilization.csv";
 	style=$(( $i+1 ));
 
-	wget -q --user=$USER --password=$PASSWORD -O $esutilization_filename $CPU "$SERVER/api/v7/timeseries?query=select utilization where category = disk and hostname=$nodename and logicalPartition = false&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
+	wget -q --user=$USER --password=$PASSWORD -O $ioutilization_filename $CPU "$SERVER/api/v7/timeseries?query=select utilization where category = disk and hostname=$nodename and logicalPartition = false&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
 	wget -q --user=$USER --password=$PASSWORD -O $cpuusage_filename $CPU "$SERVER/api/v7/timeseries?query=select cpu_user_rate / getHostFact(numCores, 1) * 100 %2B cpu_system_rate / getHostFact(numCores, 1) * 100 where hostname=$nodename&contentType=text/csv&from=$STARTDATE&to=$FINISHDATE&desiredRollup=RAW"
-	esplot=$esplot"\"$esutilization_filename\" using (\$0*60):4 ls $style w l title 'Disk utilization S$i'";
+	esplot=$esplot"\"$ioutilization_filename\" using (\$0*60):4 ls $style w l title 'Disk utilization S$i'";
 	cpuplot=$cpuplot"\"$cpuusage_filename\" using (\$0*60):4 ls $style w l title 'CPU usage S$i'";
 
 	if [ $i != $# ]; then
@@ -73,7 +82,7 @@ do
 done
 
 gnuplot << EOF
-$commonglobalplot$commonglobalesutilizationplot$esplot
+$commonglobalplot$commonglobalioutilizationplot$esplot
 EOF
 
 gnuplot << EOF
